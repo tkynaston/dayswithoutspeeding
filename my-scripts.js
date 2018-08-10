1bcea6064ee9f1bd1a9d6cf7fcd8bf916721baf8
tkynaston <42258599+tkynaston@users.noreply.github.com>
2018-08-09 20:39:41 -0600

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,31 @@
+// set count 
+if (localStorage.getItem("count") == null) {
+	localStorage.setItem("count", 0)
+}
+
+// update count
+function update(){
+	document.getElementById("count").innerHTML = localStorage.getItem("count");
+}
+
+update();
+
+//add one to count
+function plusOne() {
+	localStorage.setItem("count", Number(localStorage.getItem("count")) + 1);
+	update();
+}
+
+//minus one to count
+function minusOne() {
+	localStorage.setItem("count", Number(localStorage.getItem("count")) - 1);
+	update();
+}
+
+//reset count
+function reset() {
+	if (confirm("Are you sure you want to reset?")) {
+    	localStorage.setItem("count", 0);
+		update();
+	}
+}
